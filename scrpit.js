735a962a410089d6bf1791e1e82548262b2c40e6
Linssi Colin Ruiz <linssi101@gmail.com>
2022-11-18 01:06:27 -0600

    added event handlers to onReady, function that stores info

--- scrpit.js
+++ scrpit.js
@@ -1,7 +1,25 @@
-console.log('Working!');
-
+console.log("Working!");
 $(onReady);
 
-function onReady () {
-    console.log('Still ready!');
+function onReady() {
+	console.log("Still ready!");
+	$(".submit-button").on("click", submitInformation);
+    $(".submit-button").on('click', removeInputs);
+}
+
+// Function that stores Employee Information
+function submitInformation() {
+	console.log("working!");
+	let fName = $(".user-input-f-name").val();
+	let lName = $(".user-input-l-name").val();
+	let employeeId = $(".user-input-id").val();
+	let employeeTitle = $(".user-input-title").val();
+	let employeeAS = $(".user-input-a-s").val();
+
+	let employees = { fName, lName, employeeId, employeeTitle, employeeAS };
+	console.log(employees);
+} // end of submitInformation
+
+function removeInputs () {
+    console.log('Clicked!!!!');
 }
